56d96d87ad12009ca742dcb8abf88d6af306359a
Romain SALVAN <romain.salvan@g-mail.fr>
2021-02-08 15:06:32 +0100

    Better file structure

---
+++ brr_bot.py
@@ -0,0 +1,9 @@
+from discord.ext import commands
+
+class BrrBot(commands.Bot):
+    def __init__(self):
+        super().__init__(command_prefix=".")
+    
+    async def on_ready(self):
+        channel_test = self.get_channel(807909900671385632)
+        await channel_test.send('WeshWesh')
--- main.py
+++ main.py
@@ -1,16 +1,4 @@
-import os
-import discord
-from dotenv import load_dotenv
+from brr_bot import BrrBot
+import env
 
-load_dotenv()
-
-client = discord.Client()
-
-@client.event
-async def on_ready():
-    channel_test = client.get_channel(807909900671385632)
-    print('Logged in as {0}'.format(client.user))
-    await channel_test.send('Jsuis pret batard')
-
-
-client.run(os.getenv("TOKEN"))
+BrrBot().run(env.getToken())
